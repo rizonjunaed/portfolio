d63604e740ad7a441daa4bd0288db5b8940e4a8b
Md Junaed Hossain <79720875+rizonjunaed@users.noreply.github.com>
2021-06-29 04:16:57 -0500

    Update index.jsx

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -11,7 +11,7 @@ const MainPage = () => (
       <p>Welcome to my blog. The world is moving rapidly in IT (Information Technology), Computer etc sector. People all over the world are using internet in almost every aspect of their life. Here, I will try my best to post about everyday computer hacks and strategies. I can be reached to my social platforms for any suggestions of a new blog post.</p>
     </Box>
 
-    <Box mt={7} px={2} className="homepage-description">
+    <Box mt={3} px={2} className="homepage-description">
       <Typography component="p">
         My intention for making the blog post is to give as much ideas as possible. So, that everyone gets the simple help they needed to make their life easy. I will try to continuosly post necessary blogs regarding software programming and simple computer uses. I already have some important blog posts which can be found
         {' '}
