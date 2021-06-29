73074bda0c6cca3e55cd676a3788935b4e6968a1
Md Junaed Hossain <79720875+rizonjunaed@users.noreply.github.com>
2021-06-29 05:59:52 -0500

    fixed bugs

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -6,12 +6,12 @@ import article6 from './article6';
 import article7 from './article7';
 
 const data = {
-  1: article7,
+  1: article2,
   2: article3,
   3: article4,
-  4: article2,
+  4: article5,
   5: article6,
-  6: article5,
+  6: article7,
 };
 
 export default data;
